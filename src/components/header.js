67e7dc123b11c57d5832b2c52e5ea80b8ed1a3fc
Artem Synytsin <sh1nodartem@gmail.com>
2024-01-12 19:33:57 +0200

    implement clear all todo items

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,18 @@
 import React from "react";
 import AppContext from "../context";
 
-import styles from './header.module.css';
+import styles from "./header.module.css";
 
-const Header = () => (
+const Header = () => {
+  const { actions, todosCount } = React.useContext(AppContext);
+
+  return (
     <header className={styles.header}>
-        <h1 className={styles.title}>To do App</h1>
-        <span>Total Items {0}</span>
-        <button onClick={() => {}}>
-                Clear All
-        </button>
+      <h1 className={styles.title}>To do App</h1>
+      <span>Total Items: {todosCount}</span>
+      <button onClick={actions.removeAllTodos}>Clear All</button>
     </header>
-);
+  );
+};
 
 export default Header;
--- src/context.js
+++ src/context.js
@@ -4,14 +4,18 @@ import initialData from "./data";
 const Context = React.createContext();
 
 export const AppContextProvider = (props) => {
-   const providerValue = { // need to implement
-       todos: initialData,
-       todosCount: 0,
-       actions: {},
-   };
+  const [todos, setTodos] = React.useState(initialData);
 
-    return (
-        <Context.Provider value={providerValue}>{props.children}</Context.Provider>
-    );
+  const removeAllTodos = () => {
+    setTodos([]);
+  };
+
+  const providerValue = {
+    todosCount: todos.length,
+    actions: { removeAllTodos },
+    todos,
+  };
+
+  return <Context.Provider value={providerValue}>{props.children}</Context.Provider>;
 };
 export default Context;
