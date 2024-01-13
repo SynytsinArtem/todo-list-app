2734005e560de266e3a67dcafcd8708fbdccaba7
Artem Synytsin <sh1nodartem@gmail.com>
2024-01-13 23:10:46 +0200

    add basic sm screen support

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
+import Container from "@mui/material/Container";
+
 import { AppContextProvider } from "./context";
-import Header from './components/header';
-import List from './components/list.js';
-import styles from './App.module.css';
+import Header from "./components/header";
+import List from "./components/list.js";
 
 const AppView = () => (
-    <AppContextProvider>
-        <div className={styles.container}>
-            <Header/>
-            <List/>
-        </div>
-    </AppContextProvider>
+  <AppContextProvider>
+    <Container maxWidth="md" sx={{ py: "2em" }}>
+      <Header />
+      <List />
+    </Container>
+  </AppContextProvider>
 );
 
 export default AppView;
